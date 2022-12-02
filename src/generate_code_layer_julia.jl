334637ea612b96744963d8d9bd9f1865904d4003
Eric Heisler <8408507+ericheisler@users.noreply.github.com>
2022-12-01 22:27:33 -0700

    Update generate_code_layer_julia.jl

--- src/generate_code_layer_julia.jl
+++ src/generate_code_layer_julia.jl
@@ -448,6 +448,9 @@ function generate_named_op(IR::IR_operation_node, IRtypes::Union{IR_entry_types,
         end
         
         if use_eval
+            if type_name == "Float64"
+                type_name == "";
+            end
             code = type_name*"(evaluate_coefficient(coefficients[" * string(IR.args[2]) * "], " * string(IR.args[3]) * 
                 ", " * string(IR.args[4]) * ", " * string(IR.args[5]) * ", " * string(IR.args[6]) * 
                 ", " * string(IR.args[7]) * ", " * string(IR.args[8]) * ", "*string(IR.args[9])*", index_values))";
